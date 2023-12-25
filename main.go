d4dfa21dbdf38ff0d899773fcff40b90c996006d
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2023-12-25 11:04:16 -0300

    feat: new response api

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 )
-var Router * gin.Engine
+
+var Router *gin.Engine
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello world!",
+			"message": "Hello world",
 		})
 	})
 	r.Run()
