545c563042edb9bd1a538fbad0f2cc17b5f31e3b
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2023-12-25 13:02:06 -0300

    feat: health controller

---
+++ controllers/health.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"fmt"
+	"gin/config"
+	"gin/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+func Health(c *gin.Context) {
+
+	_, err := config.LoadEnvironmentVariables()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "error loading variables",
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	cpu, err := cpu.Percent(0, false)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "loading error cpu metrics",
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	mem, err := mem.VirtualMemory()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "loading error memory metrics",
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	uptime, err := utils.GetUptime()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": "loading error host metrics",
+			"error":  err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+		"cpu":     fmt.Sprintf("%.1f", cpu[0]),
+		"mem":     fmt.Sprintf("%.1f", mem.UsedPercent),
+		"uptime":  uptime,
+		"envs":    "load environments ok!",
+	})
+
+}
