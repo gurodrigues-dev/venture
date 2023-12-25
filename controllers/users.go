db471e9c77e174785a18953c9d0fff9c4a684128
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2023-12-25 13:02:33 -0300

    feat: crud controller

---
+++ controllers/users.go
@@ -0,0 +1,89 @@
+package controllers
+
+import (
+	"gin/models"
+	"gin/repository"
+	"gin/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func CreateUser(c *gin.Context) {
+
+	requestID, _ := c.Get("RequestID")
+
+	respOfAws, err := utils.SaveQRCodeOfUser(c.PostForm("cpf"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": respOfAws,
+			"error":   err.Error(),
+		})
+
+		return
+	}
+
+	user := models.User{
+		Name:  c.PostForm("name"),
+		CPF:   c.PostForm("cpf"),
+		RG:    c.PostForm("rg"),
+		CNH:   c.PostForm("cnh"),
+		ID:    uuid.New(),
+		Email: c.PostForm("email"),
+		URL:   respOfAws,
+	}
+
+	endereco := models.Endereco{}
+
+	validateDocs, documentError := utils.ValidateDocsDriver(user, endereco)
+
+	if !validateDocs {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "type and try insert your documents again, please.",
+			"error":   documentError,
+		})
+
+		return
+
+	}
+
+	resp, err := repository.InsertNewUser(user, endereco)
+
+	if err != nil {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "error when inserting into database",
+			"error":   err.Error(),
+		})
+
+		return
+
+	}
+
+	if resp == true {
+
+		c.JSON(http.StatusCreated, gin.H{
+			"requestID":   requestID,
+			"status":      "user created successfully",
+			"s3bucketurl": respOfAws,
+		})
+	}
+
+	return
+
+}
+
+func GetUser(c *gin.Context) {
+
+}
+
+func UpdateUser(c *gin.Context) {
+
+}
+
+func DeleteUser(c *gin.Context) {
+
+}
