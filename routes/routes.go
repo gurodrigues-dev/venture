cf692bccc2c436d9b8b277c6fb96758b47272790
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2023-12-25 13:03:09 -0300

    feat: adding new routes and applying mvc

---
+++ routes/routes.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"gin/controllers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+func HandleRequests() {
+
+	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+
+		requestID := uuid.New()
+
+		c.Writer.Header().Set("X-Request-ID", requestID.String())
+
+		c.Set("RequestID", requestID)
+
+		c.Next()
+
+	})
+
+	// health
+
+	r.GET("api/v1/health", controllers.Health)
+
+	// usuarios
+
+	r.POST("api/v1/users", controllers.CreateUser)
+
+	r.GET("api/v1/users", controllers.GetUser)
+
+	r.PUT("api/v1/users", controllers.UpdateUser)
+
+	r.DELETE("api/v1/users", controllers.DeleteUser)
+
+	r.Run()
+
+}
