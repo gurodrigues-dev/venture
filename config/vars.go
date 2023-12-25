3dfc7b0b0f9cfc548a6f8539dd8946d34f714131
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2023-12-25 13:01:16 -0300

    chore: env vars pkg

---
+++ config/vars.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvironmentVariables() (bool, error) {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading file .env")
+
+		return false, err
+	}
+
+	return true, nil
+}
+
+func GetHostDatabase() string {
+	return os.Getenv("host")
+}
+
+func GetNameDatabase() string {
+	return os.Getenv("database")
+}
+
+func GetPasswordDatabase() string {
+	return os.Getenv("password")
+}
+
+func GetPortDatabase() string {
+	return os.Getenv("port")
+}
+
+func GetUserDatabase() string {
+	return os.Getenv("user")
+}
+
+func GetRegionAws() string {
+	return os.Getenv("region")
+}
+
+func GetS3BucketAws() string {
+	return os.Getenv("bucket")
+}
+
+func GetAwsAccessKey() string {
+	return os.Getenv("accesskeyaws")
+}
+
+func GetAwsSecretKey() string {
+	return os.Getenv("secretkeyaws")
+}
+
+func GetAwsTokenKey() string {
+	return os.Getenv("tokenkeyaws")
+}
