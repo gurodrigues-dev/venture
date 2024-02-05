c7333309661bc3e4cf734ddbebcabd9fe0e39961
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-02-05 13:19:39 -0300

    refactor: repository task

---
+++ repository/school.go
@@ -0,0 +1,27 @@
+package repository
+
+import "gin/models"
+
+func SaveSchool(school *models.School) {
+
+	return
+
+}
+
+func FindSchoolByName(name *string) {
+
+	return
+
+}
+
+func UpdateSchool() {
+
+	return
+
+}
+
+func DeleteSchoolByCnpj(cpf *string) {
+
+	return
+
+}
