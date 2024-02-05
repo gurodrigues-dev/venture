820db6fc23a683f39708262ce518e5589b8351ec
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-02-05 13:51:14 -0300

    fix: school model

--- models/schools.go
+++ models/schools.go
@@ -1,11 +1,11 @@
 package models
 
 type School struct {
-	Name    string `json:"nome"`
-	CNPJ    string `json:"cnpj"`
-	Rua     string `json:"rua"`
-	Numero  string `json:"numero"`
-	CEP     string `json:"cep"`
-	Email   string `json:"email"`
-	Passwod string `json:"password"`
+	Name     string `json:"nome"`
+	CNPJ     string `json:"cnpj"`
+	Rua      string `json:"rua"`
+	Numero   string `json:"numero"`
+	CEP      string `json:"cep"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
