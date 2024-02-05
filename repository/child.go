eadf0a6890ad89067735cb332c59d15087c0b057
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-02-05 13:20:14 -0300

    refactor: repository task

---
+++ repository/child.go
@@ -0,0 +1,27 @@
+package repository
+
+import "gin/models"
+
+func SaveChild(child *models.Child) {
+
+	return
+
+}
+
+func GetChildByForeignKey(cpf *string) {
+
+	return
+
+}
+
+func UpdateChild() {
+
+	return
+
+}
+
+func DeleteChildrenByRg(rg *string) {
+
+	return
+
+}
