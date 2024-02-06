762f91ecf66125aa5827e6b6f93a081b02ff022a
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-02-06 00:34:58 -0300

    feat: jwt to users at cnpj

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,29 +1,63 @@
 package utils
 
 import (
+	"fmt"
 	"gin/config"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-func CreateJwtToken(cpf string) (string, error) {
+func checkTypeDocument(document *string) string {
+
+	switch len(*document) {
+	case 11:
+		return "CPF"
+	case 14:
+		return "CNPJ"
+	default:
+		return "invalid"
+	}
+
+}
+
+func CreateJwtToken(document string) (string, error) {
+
+	typeOfDocument := checkTypeDocument(&document)
 
 	config.LoadEnvironmentVariables()
 
 	var secretKey = config.GetSecretKeyApi()
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"cpf": cpf,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
+	if typeOfDocument == "CPF" {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"cpf": document,
+			"exp": time.Now().Add(time.Hour * 24).Unix(),
+		})
 
-	jwt, err := token.SignedString([]byte(secretKey))
+		jwt, err := token.SignedString([]byte(secretKey))
 
-	if err != nil {
-		return "", err
+		if err != nil {
+			return "", err
+		}
+
+		return jwt, nil
 	}
 
-	return jwt, nil
+	if typeOfDocument == "CNPJ" {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"cnpj": document,
+			"exp":  time.Now().Add(time.Hour * 24).Unix(),
+		})
 
+		jwt, err := token.SignedString([]byte(secretKey))
+
+		if err != nil {
+			return "", err
+		}
+
+		return jwt, nil
+	}
+
+	return "", fmt.Errorf("The type of document is invalid")
 }
