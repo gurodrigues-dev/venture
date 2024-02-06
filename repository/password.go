dce1ea0e26494f12a8015f2a79e7bb54a16dbe2b
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-02-06 00:36:08 -0300

    feat: repository for password

--- repository/password.go
+++ repository/password.go
@@ -81,3 +81,43 @@ func VerifyPasswordByCpf(cpf, table, hash string) (bool, error) {
 	return passwordMatch, nil
 
 }
+
+func VerifyPasswordByCnpj(cnpj, typeSchool, hash *string) (bool, error) {
+
+	_, err := config.LoadEnvironmentVariables()
+
+	if err != nil {
+		return false, err
+	}
+
+	var (
+		userdb   = config.GetUserDatabase()
+		port     = config.GetPortDatabase()
+		host     = config.GetHostDatabase()
+		password = config.GetPasswordDatabase()
+		dbname   = config.GetNameDatabase()
+	)
+
+	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, userdb, password, dbname)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	var storedHashedPassword string
+
+	query := "SELECT password FROM " + *typeSchool + " WHERE cnpj = $1"
+
+	err = db.QueryRow(query, *cnpj).Scan(&storedHashedPassword)
+	if err != nil {
+		return false, err
+	}
+
+	passwordMatch := storedHashedPassword == *hash
+
+	return passwordMatch, nil
+
+}
