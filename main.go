6dace33a681fbb0b26356b7a13a9d96199a50a43
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-14 08:53:02 -0300

    refactor: adapting to mvc

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"gin/routes"
-
-	"github.com/gin-gonic/gin"
+	"gin/config"
+	"gin/internal/controllers"
+	"gin/internal/repository"
+	"gin/internal/service"
+	"log"
 )
-
-var Router *gin.Engine
 
 func main() {
 
-	routes.HandleRequests()
+	config, err := config.Load("config/config.yaml")
+	if err != nil {
+		log.Fatalf("error loading config: %s", err.Error())
+	}
+
+	repo, err := repository.NewPostgres()
+	if err != nil {
+		log.Fatalf("error creating repository: %s", err.Error())
+	}
+
+	aws, err := repository.NewAwsConnection()
+	if err != nil {
+		log.Fatalf("error creating aws connection: %s", err.Error())
+	}
+
+	redis, err := repository.NewRedisClient()
+	if err != nil {
+		log.Fatalf("error creating redis connection: %s", err.Error())
+	}
+
+	service := service.New(repo, aws, redis)
+
+	controller := controllers.New(service)
+
+	log.Printf("initing service: %s", config.Name)
+	controller.Start()
 
 }
