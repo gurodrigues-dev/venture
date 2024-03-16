74045ede60fe8d66ba21204bdfb1f8ffce6b1c68
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-16 15:27:46 -0300

    feat: service struct

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,9 @@
 package service
 
-import "gin/internal/repository"
+import (
+	"context"
+	"gin/internal/repository"
+)
 
 type Service struct {
 	repository repository.Repository
@@ -14,4 +17,87 @@ func New(repo repository.Repository, cloud repository.CloudRepository, redis rep
 		cloud:      cloud,
 		redis:      redis,
 	}
+}
+
+func (s *Service) CreateUser(ctx context.Context) {
+
+}
+
+func (s *Service) ReadUser(ctx context.Context) {
+
+}
+
+func (s *Service) UpdateUser(ctx context.Context) {
+
+}
+
+func (s *Service) DeleteUser(ctx context.Context) {
+
+}
+
+func (s *Service) CreateChild(ctx context.Context) {
+
+}
+
+func (s *Service) ReadChild(ctx context.Context) {
+
+}
+
+func (s *Service) UpdateChild(ctx context.Context) {
+
+}
+
+func (s *Service) DeleteChild(ctx context.Context) {
+
+}
+
+func (s *Service) CreateDriver(ctx context.Context) {
+
+}
+
+func (s *Service) ReadDriver(ctx context.Context) {
+
+}
+
+func (s *Service) UpdateDriver(ctx context.Context) {
+
+}
+func (s *Service) DeleteDriver(ctx context.Context) {
+
+}
+
+func (s *Service) CreateSchool(ctx context.Context) {
+
+}
+
+func (s *Service) ReadScCreateSchool(ctx context.Context) {
+
+}
+
+func (s *Service) UpdateScCreateSchool(ctx context.Context) {
+
+}
+
+func (s *Service) DeleteScCreateSchool(ctx context.Context) {
+
+}
+
+func (s *Service) CheckEmail(ctx context.Context) {
+
+}
+
+func (s *Service) SendEmail(ctx context.Context) {
+
+}
+
+func (s *Service) SaveImageBucket(ctx context.Context) {
+
+}
+
+func (s *Service) SaveKeyAndValue(ctx context.Context) {
+
+}
+
+func (s *Service) VerifyToken(ctx context.Context) {
+
 }
--- types/types.go
+++ types/types.go
@@ -1,1 +1,8 @@
 package types
+
+type School struct {
+	Name     string `json:"name"`
+	CNPJ     string `json:"cnpj"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
