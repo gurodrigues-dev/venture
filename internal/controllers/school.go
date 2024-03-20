a81901dceb7cb84a2585a57b33a0a07c0d25daac
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-20 00:23:22 -0300

    feat: new controller of school

--- internal/controllers/school.go
+++ internal/controllers/school.go
@@ -1,8 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin/types"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (ct *controller) CreateSchool(c *gin.Context) {
+
+	var input types.School
+
+	if err := c.BindJSON(&input); err != nil {
+		log.Printf("error parsing body content: %s", err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid body content"})
+	}
 
 }
 
@@ -15,5 +28,9 @@ func (ct *controller) UpdateSchool(c *gin.Context) {
 }
 
 func (ct *controller) DeleteSchool(c *gin.Context) {
+
+}
+
+func (ct *controller) AuthSchool(c *gin.Context) {
 
 }
