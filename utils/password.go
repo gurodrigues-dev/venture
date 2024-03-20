13d1024c2648ac1c56e993c50780ccf6e89ee923
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-20 00:22:48 -0300

    feat: adding utils

---
+++ utils/password.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func HashPassword(password string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(password))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
