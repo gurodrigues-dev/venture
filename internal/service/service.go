03415abaa3e9b9ad9f6a7bd1af74edc1cd3d302a
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-22 01:09:31 -0300

    build: adding new lib

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,9 +2,15 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"gin/config"
 	"gin/internal/repository"
 	"gin/types"
 	"gin/utils"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 )
 
 type Service struct {
@@ -86,7 +92,7 @@ func (s *Service) DeleteCreateSchool(ctx context.Context, id *int) error {
 	return s.repository.DeleteSchool(ctx, id)
 }
 
-func (s *Service) AuthSchool(ctx context.Context, school *types.School) error {
+func (s *Service) AuthSchool(ctx context.Context, school *types.School) (*types.School, error) {
 	return s.repository.AuthSchool(ctx, school)
 }
 
@@ -107,5 +113,84 @@ func (s *Service) SaveKeyAndValue(ctx context.Context) {
 }
 
 func (s *Service) VerifyToken(ctx context.Context) {
+
+}
+
+func (s *Service) CreateTokenJWTUser(ctx context.Context, user *types.User) (string, error) {
+
+	conf := config.Get()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"cpf": user.CPF,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	jwt, err := token.SignedString([]byte(conf.Server.Secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return jwt, nil
+}
+
+func (s *Service) CreateTokenJWTDriver(ctx context.Context, driver *types.Driver) (string, error) {
+
+	conf := config.Get()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"cpf": driver.CPF,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	jwt, err := token.SignedString([]byte(conf.Server.Secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return jwt, nil
+}
+
+func (s *Service) CreateTokenJWTSchool(ctx context.Context, school *types.School) (string, error) {
+
+	conf := config.Get()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"cnpj": school.CNPJ,
+		"exp":  time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	jwt, err := token.SignedString([]byte(conf.Server.Secret))
+
+	if err != nil {
+		return "", err
+	}
+
+	return jwt, nil
+
+}
+
+func (s *Service) ParserJwtSchool(ctx *gin.Context) error {
+
+	_, found := ctx.Get("")
+
+	if !found {
+		return fmt.Errorf("error while veryfing token")
+	}
+
+	return nil
+
+}
+
+func (s *Service) ParserJwtUserAndDriver(ctx *gin.Context) error {
+
+	_, found := ctx.Get("")
+
+	if !found {
+		return fmt.Errorf("error while veryfing token")
+	}
+
+	return nil
 
 }
