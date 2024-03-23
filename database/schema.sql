50fccced3df532db46a9b75f6b509d8029e6dae5
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-03-23 16:40:39 -0300

    refactor: schema change language

--- database/schema.sql
+++ database/schema.sql
@@ -21,41 +21,41 @@ CREATE TABLE IF NOT EXISTS drivers (
 CREATE TABLE IF NOT EXISTS address (
     id SERIAL PRIMARY KEY,
     cpf VARCHAR(14) REFERENCES users(cpf),
-    rua VARCHAR(100) NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    estado VARCHAR(2) NOT NULL,
-    cep VARCHAR(8) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    complemento VARCHAR(100)
+    street VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(2) NOT NULL,
+    zip VARCHAR(8) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    complement VARCHAR(100)
 );
 
 -- Tabela children
 CREATE TABLE IF NOT EXISTS childrens (
     id SERIAL,
     rg VARCHAR(20) PRIMARY KEY,
-    responsavel VARCHAR(14),
-    nome VARCHAR(100),
-    escola VARCHAR(100),
+    responsible VARCHAR(14),
+    name VARCHAR(100),
+    school VARCHAR(100),
     driver VARCHAR(100),
-    endereco TEXT,
-    FOREIGN KEY (responsavel) REFERENCES drivers(cpf)
+    address TEXT,
+    FOREIGN KEY (responsible) REFERENCES users(cpf)
 );
 
 -- Tabela schools
 CREATE TABLE IF NOT EXISTS schools (
     id SERIAL,
-    nome VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
     cnpj VARCHAR(14) PRIMARY KEY,
-    rua VARCHAR(100) NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    cep VARCHAR(8) NOT NULL,
+    street VARCHAR(100) NOT NULL,
+    number VARCHAR(10) NOT NULL,
+    zip VARCHAR(8) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
 
 -- Tabela school_drivers
 CREATE TABLE IF NOT EXISTS schools_drivers (
-    registro SERIAL PRIMARY KEY,
+    record SERIAL PRIMARY KEY,
     school VARCHAR(14),
     driver VARCHAR(14),
     FOREIGN KEY (school) REFERENCES schools(cnpj),
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS schools_drivers (
 
 -- Tabela users_drivers
 CREATE TABLE IF NOT EXISTS users_drivers (
-    matricula SERIAL PRIMARY KEY,
+    registration SERIAL PRIMARY KEY,
     driver VARCHAR(14),
     child VARCHAR(20),
     FOREIGN KEY (driver) REFERENCES drivers(cpf),
