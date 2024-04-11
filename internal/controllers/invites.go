803601648704db60537fbe09a581b432fbf73975
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-04-11 08:31:19 -0300

    feat: create controllers to invites

---
+++ internal/controllers/invites.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func (ct *controller) CreateInvite(c *gin.Context) {
+
+}
+
+func (ct *controller) ReadInvite(c *gin.Context) {
+
+}
+
+func (ct *controller) UpdateInvite(c *gin.Context) {
+
+}
+
+func (ct *controller) DeleteInvite(c *gin.Context) {
+
+}
