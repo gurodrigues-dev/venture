7a375c7fd79f16c1755567f5d77e4388eb30fc5a
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-04-15 22:44:07 -0300

    build: new dependencies

--- internal/repository/aws.go
+++ internal/repository/aws.go
@@ -1,12 +1,16 @@
 package repository
 
 import (
+	"bytes"
 	"context"
+	"fmt"
 	"gin/config"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/skip2/go-qrcode"
 )
 
 type AWS struct {
@@ -34,6 +38,35 @@ func NewAwsConnection() (*AWS, error) {
 
 }
 
-func (a *AWS) SaveImageBucket(ctx context.Context) {
+func (a *AWS) SaveImageBucket(ctx context.Context, cpf *string) (string, error) {
+
+	conf := config.Get()
+
+	qrCodeData := fmt.Sprintf("http://localhost:8080/api/v1/drivers/%s", *cpf)
+	qrCode, err := qrcode.Encode(qrCodeData, qrcode.Medium, 256)
+
+	if err != nil {
+		return "Error creating qrcode", err
+	}
+
+	svc := s3.New(a.conn)
+
+	fileName := fmt.Sprintf("qrcodes/%s.png", *cpf)
+
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket:      aws.String(conf.Cloud.BucketName),
+		Key:         aws.String(fileName),
+		Body:        bytes.NewReader(qrCode),
+		ACL:         aws.String("public-read"),
+		ContentType: aws.String("image/png"),
+	})
+
+	if err != nil {
+		return "Error while saving qrcode in aws", err
+	}
+
+	qrCodeURL := fmt.Sprintf("https://%s.s3.amazonaws.com/%s", conf.Cloud.BucketName, fileName)
+
+	return qrCodeURL, nil
 
 }
