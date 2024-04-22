0acfefcb5ae387ee5cbdbcde9e8e1f38ac0503ff
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-04-21 22:30:18 -0300

    feat: new funcs to psql about invites

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -280,18 +280,128 @@ func (p *Postgres) NewPassword(ctx context.Context) {
 
 }
 
-func (p *Postgres) CreateInvite(ctx context.Context) {
+func (p *Postgres) CreateInvite(ctx context.Context, invite *types.Invite) error {
+	sqlQuery := `INSERT INTO invites (requester, guest, status) VALUES ($1, $2, $3)`
+	_, err := p.conn.Exec(sqlQuery, invite.Requester, invite.Guest, "pending")
+	return err
+}
+
+func (p *Postgres) ReadInvite(ctx context.Context, invite_id *int) (*types.Invite, error) {
+	sqlQuery := `SELECT invite_id, requester, guest, status FROM invites WHERE invite_id = $1 LIMIT 1`
+	var invite types.Invite
+	err := p.conn.QueryRow(sqlQuery, *invite_id).Scan(
+		&invite.ID,
+		&invite.Requester,
+		&invite.Guest,
+		&invite.Status,
+	)
+	if err != nil || err == sql.ErrNoRows {
+		return nil, err
+	}
+	return &invite, nil
+}
+
+func (p *Postgres) ReadAllInvites(ctx context.Context, cnh *string) ([]types.Invite, error) {
+	sqlQuery := `SELECT invite_id, requester, status FROM invites WHERE status = pending AND guest = $1`
+
+	rows, err := p.conn.Query(sqlQuery, *cnh)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var invites []types.Invite
+
+	for rows.Next() {
+		var invite types.Invite
+		err := rows.Scan(&invite.ID, &invite.Requester, &invite.Guest, &invite.Status)
+		if err != nil {
+			return nil, err
+		}
+		invites = append(invites, invite)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return invites, nil
 
 }
 
-func (p *Postgres) ReadInvite(ctx context.Context) {
+func (p *Postgres) UpdateInvite(ctx context.Context, invite_id *int) error {
+	sqlQuery := `UPDATE invites SET status = accepting WHERE invite_id = $1`
+	_, err := p.conn.Exec(sqlQuery, invite_id)
 
+	return err
 }
 
-func (p *Postgres) UpdateInvite(ctx context.Context) {
-
+func (p *Postgres) DeleteInvite(ctx context.Context, invite_id *int) error {
+	tx, err := p.conn.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+	_, err = tx.Exec("DELETE FROM invites WHERE invite_id = $1", invite_id)
+	return err
 }
 
-func (p *Postgres) DeleteInvite(ctx context.Context) {
+func (p *Postgres) CreateRecordToSchoolAndDriver(ctx context.Context, invite *types.Invite) error {
+	sqlQuery := `INSERT INTO schools_drivers (school, driver) VALUES ($1, $2)`
+	_, err := p.conn.Exec(sqlQuery, invite.Requester, invite.Guest)
+	return err
+}
+
+func (p *Postgres) DeleteRecordToSchoolAndDriver(ctx context.Context, record_id *int) error {
+	tx, err := p.conn.Begin()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+	_, err = tx.Exec("DELETE FROM invites WHERE invite_id = $1", record_id)
+	return err
+}
+
+func (p *Postgres) GetDriversSchool(ctx context.Context, cnh *string) ([]types.School, error) {
+	sqlQuery := `SELECT school FROM schools_drivers WHERE driver = $1`
+
+	rows, err := p.conn.Query(sqlQuery, *cnh)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var schools []types.School
+
+	for rows.Next() {
+		var school types.School
+		err := rows.Scan(&school.CNPJ)
+		if err != nil {
+			return nil, err
+		}
+		schools = append(schools, school)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return schools, nil
 
 }
