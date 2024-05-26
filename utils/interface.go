08c8e421f31d527d94fca42fafa751e9003687bb
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-05-26 20:35:57 -0300

    refactor: utils

--- utils/interface.go
+++ utils/interface.go
@@ -3,10 +3,14 @@ package utils
 import "fmt"
 
 func InterfaceToString(value interface{}) (*string, error) {
+
 	switch v := value.(type) {
+
 	case string:
 		return &v, nil
+
 	default:
 		return nil, fmt.Errorf("value isn't string")
 	}
+
 }
