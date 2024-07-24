5d2b923c897acaf29b77eecc69e83f67be4d52b3
Gustavo Rodrigues <gustavorodrigueslima2004@gmail.com>
2024-07-24 00:07:21 -0300

    feat: struct about invoice and subscription

--- types/stripe.go
+++ types/stripe.go
@@ -4,3 +4,22 @@ type StripeEnv struct {
 	PublicKey string
 	SecretKey string
 }
+
+type SubscriptionInfo struct {
+	ID     string
+	Status string
+}
+
+type InvoiceInfo struct {
+	ID              string
+	Status          string
+	AmountDue       int64
+	AmountRemaining int64
+}
+
+type InvoiceRemaining struct {
+	InvoiceValue float64
+	Quantity     float64
+	Remaining    float64
+	Fines        float64
+}
